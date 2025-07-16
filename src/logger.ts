6b1e0810cc5b8577cbd9cfb1fe2f9cbf14c41d32
Rahul Shah <rahul.shah@synergyship.com>
2025-07-16 18:23:36 +0530

    updated get_vessel_details to accept different args for vessel name

---
+++ src/logger.ts
@@ -0,0 +1,25 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console({
+            stderrLevels: ['error', 'warn', 'info', 'debug'],
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({ 
+            filename: 'logs/error.log', 
+            level: 'error' 
+        }),
+        new winston.transports.File({ 
+            filename: 'logs/combined.log' 
+        })
+    ]
+}); 
